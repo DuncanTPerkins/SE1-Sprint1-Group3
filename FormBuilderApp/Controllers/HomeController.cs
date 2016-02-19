c8966fefd137722d3303d9806e1ff469b728e9cc
David Shin <shind1@goldmail.etsu.edu>
2016-02-19 16:46:18 -0500

    Re-update for DummyForm View page

--- FormBuilderApp/Controllers/HomeController.cs
+++ FormBuilderApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FormBuilderApp.DataContexts;
+using FormBuilderApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace FormBuilderApp.Controllers
 {
     public class HomeController : Controller
     {
+        private FormBuilderDb _db = new FormBuilderDb();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,22 @@ namespace FormBuilderApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles="user")]
+        public ActionResult DummyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "user")]
+        public ActionResult DummyForm(Form form1)
+        {
+            _db.Forms.Add(form1);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
