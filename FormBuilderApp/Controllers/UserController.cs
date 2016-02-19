a65b5f0bbd95b12a6f230197e87b369c3b3b217c
Duncan Perkins <perkinsdt@goldmail.etsu.edu>
2016-02-18 21:54:14 -0500

    Account tables, test users, and role page proof of concept

---
+++ FormBuilderApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FormBuilderApp.DataContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormBuilderApp.Controllers
+{
+    public class UserController : Controller
+    {
+        IdentityDb _identityDb = new IdentityDb();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Roles()
+        {
+            return View(_identityDb.Roles.ToList());
+        }
+    }
+}
