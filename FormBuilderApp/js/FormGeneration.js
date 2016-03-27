d5a47896e47063f108e523e9c500e9a6cda3f56d
Haley Hughes <hugheshe1@goldmail.etsu.edu>
2016-03-26 21:49:48 -0400

    Implemented functionality to allow user to create options for the checkbox, dropdown, and radio form elements. Also added delete buttons for newly added option fields but delete functionality does not yet work.

--- FormBuilderApp/js/FormGeneration.js
+++ FormBuilderApp/js/FormGeneration.js
@@ -23,6 +23,8 @@ var textAreaTmpl = _.template($('#textAreaTmpl').text());
 var checkboxTmpl = _.template($('#checkboxTmpl').text());
 var radioTmpl = _.template($('#radioTmpl').text());
 var dropdownTmpl = _.template($('#dropdownTmpl').text());
+var optionsTmpl = _.template($('#optionsTmpl').text());
+var optionField = _.template($('#optionFieldTmpl').text());
 
 // Field Builder submit handler
 $fieldForm.on('submit', function (event) {
@@ -32,16 +34,66 @@ $fieldForm.on('submit', function (event) {
     var fieldName = $nameInput.val();
     var isRequired = $requiredInput.prop('checked');
     var placeholder = $placeholderInput.val();
+    var options = $.map($('.field-option'), function (el) {
+        return $(el).val()
+    });
 
-    formData.fields.push({
+    var fieldData = {
         type: fieldType,
         name: fieldName,
         required: isRequired,
-        placeholder: placeholder
-    })
+        placeholder: placeholder,
+        options: options
+    }
 
+    formData.fields.push(fieldData);
+
+    console.log(fieldData);
+
+    resetFieldBuilder();
     renderFormPreview();
 });
+
+function resetFieldBuilder() {
+    $fieldForm[0].reset();
+    $('.placeholder-container').removeClass('hidden');
+    $('#field-options').empty();
+}
+
+// Listen to changes on type selection
+$typeInput.on('change', function (e) {
+    var selectedType = $(this).val();
+    var hasPlaceholder = true;
+    var hasOptions = false;
+
+    var isSelect = selectedType === 'select';
+    var isRadio = selectedType === 'radio';
+    var isCheckbox = selectedType === 'checkbox';
+
+    // Render options field in field builder if type is select
+    if (isSelect || isRadio || isCheckbox) {
+        hasPlaceholder = false;
+        hasOptions = true;
+        renderOptionsField()
+    }
+
+    $('.placeholder-container').toggleClass('hidden', !hasPlaceholder);
+    if (!hasOptions) {
+        $('#field-options').empty();
+    }
+});
+
+function renderOptionsField() {
+    $('#field-options').html(optionsTmpl())
+
+    var $addBtn = $('#add-option');
+    var $list = $('#option-list');
+
+    // Add new option input when 'Add' button is clicked
+    $addBtn.on('click', function (e) {
+        $list.append(optionField())
+    })
+}
 
 function renderFormPreview() {
     var $formContent = $(formTmpl(formData));
