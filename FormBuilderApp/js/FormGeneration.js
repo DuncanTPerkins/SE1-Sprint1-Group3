7a309d1b795bacc78ad51aec19a013daf8f24b82
Haley Hughes <hugheshe1@goldmail.etsu.edu>
2016-03-27 02:46:30 -0400

    Allow form data object to be sent to server.

--- FormBuilderApp/js/FormGeneration.js
+++ FormBuilderApp/js/FormGeneration.js
@@ -1,11 +1,12 @@
 ﻿var formData = {
-    name: null,
-    fields: []
+  name: null,
+  fields: []
 }
 
 //Search through the DOM for any elements that match the
 //provided selector and create a new jQuery object that 
 //references each element
+var $createBtn = $('#create');
 var $preview = $('#preview');
 var $fieldForm = $('form[name=builder]')
 var $typeInput = $('#typeInput');
@@ -27,102 +28,116 @@ var optionsTmpl = _.template($('#optionsTmpl').text());
 var optionField = _.template($('#optionFieldTmpl').text());
 
 // Field Builder submit handler
-$fieldForm.on('submit', function (event) {
-    event.preventDefault();
-
-    var fieldType = $typeInput.val();
-    var fieldName = $nameInput.val();
-    var isRequired = $requiredInput.prop('checked');
-    var placeholder = $placeholderInput.val();
-    var options = $.map($('.field-option'), function (el) {
-        return $(el).val()
-    });
-
-    var fieldData = {
-        type: fieldType,
-        name: fieldName,
-        required: isRequired,
-        placeholder: placeholder,
-        options: options
-    }
-
-    formData.fields.push(fieldData);
-
-    console.log(fieldData);
-
-    resetFieldBuilder();
-    renderFormPreview();
+$fieldForm.on('submit', function(event) {
+  event.preventDefault();
+  
+  var fieldType = $typeInput.val();
+  var fieldName = $nameInput.val();
+  var isRequired = $requiredInput.prop('checked');
+  var placeholder = $placeholderInput.val();
+  var options = $.map($('.field-option'), function (el) {
+    return $(el).val()
+  });
+  
+  var fieldData = {
+    type: fieldType,
+    name: fieldName,
+    required: isRequired,
+    placeholder: placeholder,
+    options: options
+  }
+  
+  formData.fields.push(fieldData);
+  
+  console.log(fieldData);
+  
+  
+  resetFieldBuilder();
+  renderFormPreview();
 });
 
 function resetFieldBuilder() {
-    $fieldForm[0].reset();
-    $('.placeholder-container').removeClass('hidden');
-    $('#field-options').empty();
+  $fieldForm[0].reset();
+  $('.placeholder-container').removeClass('hidden');
+  $('#field-options').empty();
 }
 
 // Listen to changes on type selection
-$typeInput.on('change', function (e) {
-    var selectedType = $(this).val();
-    var hasPlaceholder = true;
-    var hasOptions = false;
-
-    var isSelect = selectedType === 'select';
-    var isRadio = selectedType === 'radio';
-    var isCheckbox = selectedType === 'checkbox';
-
-    // Render options field in field builder if type is select
-    if (isSelect || isRadio || isCheckbox) {
-        hasPlaceholder = false;
-        hasOptions = true;
-        renderOptionsField()
-    }
-
-    $('.placeholder-container').toggleClass('hidden', !hasPlaceholder);
-    if (!hasOptions) {
-        $('#field-options').empty();
-    }
+$typeInput.on('change', function(e) {
+  var selectedType = $(this).val();
+  var hasPlaceholder = true;
+  var hasOptions = false;
+  
+  var isSelect = selectedType === 'select';
+  var isRadio = selectedType === 'radio';
+  var isCheckbox = selectedType === 'checkbox';
+  
+  // Render options field in field builder if type is select
+  if (isSelect || isRadio || isCheckbox) {
+    hasPlaceholder = false;
+    hasOptions = true;
+    renderOptionsField()
+  }
+  
+  $('.placeholder-container').toggleClass('hidden', !hasPlaceholder);
+  if (!hasOptions) {
+    $('#field-options').empty();
+  }
 });
 
 function renderOptionsField() {
-    $('#field-options').html(optionsTmpl())
-
-    var $addBtn = $('#add-option');
-    var $list = $('#option-list');
-
-    // Add new option input when 'Add' button is clicked
-    $addBtn.on('click', function (e) {
-        $list.append(optionField())
-    })
+  $('#field-options').html(optionsTmpl())
+    
+  var $addBtn = $('#add-option');
+  var $list = $('#option-list');
+  
+  // Add new option input when 'Add' button is clicked
+  $addBtn.on('click', function (e) {
+    $list.append(optionField())
+  })
 }
 
 function renderFormPreview() {
-    var $formContent = $(formTmpl(formData));
-    var $formFields = $formContent.find('#fields');
-    formData.fields.forEach(function (field) {
-        $formFields.append(renderField(field));
-    })
-    $preview.html($formContent);
+  var $formContent = $(formTmpl(formData));
+  var $formFields = $formContent.find('#fields');
+  formData.fields.forEach(function (field) {
+    $formFields.append(renderField(field));
+  })
+  $preview.html($formContent);
 }
 
 function renderField(fieldData) {
-    switch (fieldData.type) {
-        case 'input':
-            return inputTmpl(fieldData)
-        case 'email':
-            return emailTmpl(fieldData)
-        case 'password':
-            return passwordTmpl(fieldData)
-        case 'tel':
-            return phoneTmpl(fieldData)
-        case 'textarea':
-            return textAreaTmpl(fieldData)
-        case 'checkbox':
-            return checkboxTmpl(fieldData)
-        case 'radio':
-            return radioTmpl(fieldData)
-        case 'select':
-            return dropdownTmpl(fieldData)
-        default:
-            return;
-    }
+  switch (fieldData.type) {
+    case 'input':
+      return inputTmpl(fieldData)
+    case 'email':
+      return emailTmpl(fieldData)
+    case 'password':
+      return passwordTmpl(fieldData)
+    case 'tel':
+      return phoneTmpl(fieldData)
+    case 'textarea':
+      return textAreaTmpl(fieldData)
+    case 'checkbox':
+      return checkboxTmpl(fieldData)
+    case 'radio':
+      return radioTmpl(fieldData)
+    case 'select':
+      return dropdownTmpl(fieldData)
+    default:
+      return;
+  }
 }
+
+function submit(e) {
+  var url = '/form/create'
+  $.post(url, formData, function (data) {
+    if (data.error) {
+      alert('Error saving form. Try again later.');
+    } else {
+      alert('Form saved successfully!');
+    }
+  })
+}
+
+$createBtn.on('click', submit);
