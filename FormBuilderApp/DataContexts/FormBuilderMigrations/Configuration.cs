c6fb55eafa1b7ad252c8e9220a6703b3d4ce44e8
Ariel Rowland <rowlandan@etsu.edu>
2016-04-23 16:56:09 -0400

    deployment success?

--- FormBuilderApp/DataContexts/FormBuilderMigrations/Configuration.cs
+++ FormBuilderApp/DataContexts/FormBuilderMigrations/Configuration.cs
@@ -34,6 +34,7 @@ namespace FormBuilderApp.DataContexts.FormBuilderMigrations
                     Name = "Survey",
                     Status = Form.FormStatus.Draft,
                     FormData = "<form name='genform'><div id='fields'><div class='form-group'><label for='Club Name'>Club Name</label><input class='form-control' type='text' name='Club Name' placeholder=''></div><div class='form-group'><label>Club Type</label><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Interests'> Interests</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Social'> Social</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Sports'> Sports</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Departmental'> Departmental</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Arts'> Arts</label></div></div><div class='form-group'><label for='Staff Liaison'>Staff Liaison</label><input class='form-control' type='text' name='Staff Liaison' placeholder=''></div><div class='form-group'><label for='Charter'>Charter</label><textarea class='form-control' type='textarea' name='Charter' placeholder='' <='' div=''></textarea></div></div><button id='later' class='btn btn-danger' type='submit' name='submit' value='Draft'>Save Draft</button><button id='completed' class='btn btn-primary' type='submit' name='submit' value='Complete'>Submit</button></form>",
+                    FormObjectRepresentation = "[{'name':'Club Name','value':'Sculpting Club'},{'name':'Club Type','value':'Arts'},{'name':'Staff Liaison','value':'Burt Reynolds'},{'name':'Charter','value':''}]",
                     WorkflowId = 3,
                 },
 
@@ -44,6 +45,7 @@ namespace FormBuilderApp.DataContexts.FormBuilderMigrations
                     Name = "Survey",
                     Status = Form.FormStatus.Completed,
                     FormData = "<form name='genform'><div id='fields'><div class='form-group'><label for='Club Name'>Club Name</label><input class='form-control' type='text' name='Club Name' placeholder=''></div><div class='form-group'><label>Club Type</label><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Interests'> Interests</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Social'> Social</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Sports'> Sports</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Departmental'> Departmental</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Arts'> Arts</label></div></div><div class='form-group'><label for='Staff Liaison'>Staff Liaison</label><input class='form-control' type='text' name='Staff Liaison' placeholder=''></div><div class='form-group'><label for='Charter'>Charter</label><textarea class='form-control' type='textarea' name='Charter' placeholder='' <='' div=''></textarea></div></div><button id='later' class='btn btn-danger' type='submit' name='submit' value='Draft'>Save Draft</button><button id='completed' class='btn btn-primary' type='submit' name='submit' value='Complete'>Submit</button></form>",
+                    FormObjectRepresentation = "[{'name':'Club Name','value':'Sculpting Club'},{'name':'Club Type','value':'Arts'},{'name':'Staff Liaison','value':'Burt Reynolds'},{'name':'Charter','value':'A  bunch of rules and stuff here.'}]",
                     WorkflowId = 3,
                 },
 
@@ -54,6 +56,7 @@ namespace FormBuilderApp.DataContexts.FormBuilderMigrations
                     Name = "Survey",
                     Status = Form.FormStatus.Accepted,
                     FormData = "<form name='genform'><div id='fields'><div class='form-group'><label for='Club Name'>Club Name</label><input class='form-control' type='text' name='Club Name' placeholder=''></div><div class='form-group'><label>Club Type</label><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Interests'> Interests</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Social'> Social</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Sports'> Sports</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Departmental'> Departmental</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Arts'> Arts</label></div></div><div class='form-group'><label for='Staff Liaison'>Staff Liaison</label><input class='form-control' type='text' name='Staff Liaison' placeholder=''></div><div class='form-group'><label for='Charter'>Charter</label><textarea class='form-control' type='textarea' name='Charter' placeholder='' <='' div=''></textarea></div></div><button id='later' class='btn btn-danger' type='submit' name='submit' value='Draft'>Save Draft</button><button id='completed' class='btn btn-primary' type='submit' name='submit' value='Complete'>Submit</button></form>",
+                    FormObjectRepresentation = "[{'name':'Club Name','value':'Sculpting Club'},{'name':'Club Type','value':'Arts'},{'name':'Staff Liaison','value':'Burt Reynolds'},{'name':'Charter','value':'A  bunch of rules and stuff here.'}]",
                     WorkflowId = null,
                 },
 
@@ -64,6 +67,7 @@ namespace FormBuilderApp.DataContexts.FormBuilderMigrations
                     Name = "Survey",
                     Status = Form.FormStatus.Denied,
                     FormData = "<form name='genform'><div id='fields'><div class='form-group'><label for='Club Name'>Club Name</label><input class='form-control' type='text' name='Club Name' placeholder=''></div><div class='form-group'><label>Club Type</label><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Interests'> Interests</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Social'> Social</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Sports'> Sports</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Departmental'> Departmental</label></div><div class='radio'><label><input class='radio' type='radio' name='Club Type' value='Arts'> Arts</label></div></div><div class='form-group'><label for='Staff Liaison'>Staff Liaison</label><input class='form-control' type='text' name='Staff Liaison' placeholder=''></div><div class='form-group'><label for='Charter'>Charter</label><textarea class='form-control' type='textarea' name='Charter' placeholder='' <='' div=''></textarea></div></div><button id='later' class='btn btn-danger' type='submit' name='submit' value='Draft'>Save Draft</button><button id='completed' class='btn btn-primary' type='submit' name='submit' value='Complete'>Submit</button></form>",
+                    FormObjectRepresentation = "[{'name':'Club Name','value':'Sculpting Club'},{'name':'Club Type','value':'Arts'},{'name':'Staff Liaison','value':'Burt Reynolds'},{'name':'Charter','value':'A  bunch of rules and stuff here.'}]",
                     WorkflowId = 3,
                 }
 
@@ -75,41 +79,40 @@ namespace FormBuilderApp.DataContexts.FormBuilderMigrations
                 {
                     FlowId = 3,
                     FormId = 1,
-                    Positions = new List<Positions>
-                    {
-                        new Positions
-                        {
-                            Id = 1,
-                            Position = "President"
-                        },
-
-                        new Positions
-                        {
-                            Id = 2,
-                            Position = "Chairman"
-                        },
-
-                        new Positions
-                        {
-                            Id = 3,
-                            Position = "Dean"
-                        },
-
-                        new Positions
-                        {
-                            Id = 4,
-                            Position = "Project Manager"
-                        },
-
-                        new Positions
-                        {
-                            Id = 5,
-                            Position = "Team Leader"
-                        }
-                    }
                 }
             );
 
+            context.position.AddOrUpdate(p => p.Id,
+                new Positions
+                {
+                    Id = 1,
+                    Position = "President"
+                },
+
+                new Positions
+                {
+                    Id = 2,
+                    Position = "Chairman"
+                },
+
+                new Positions
+                {
+                    Id = 3,
+                    Position = "Dean"
+                },
+
+                new Positions
+                {
+                    Id = 4,
+                    Position = "Project Manager"
+                },
+
+                new Positions
+                {
+                    Id = 5,
+                    Position = "Team Leader"
+                }
+            );
 
         }
     }
